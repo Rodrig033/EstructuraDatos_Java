4062aff745546586f7c54a00b9006251c45859f9
Rodrigo Farid <rodrigolopezcordoba805@gmail.com>
2025-12-08 20:16:12 -0600

    Cola tareas

---
+++ src/main/java/com/mx/curso/unidad3/Practica_Estructura_Dinamica/ColaTareas.java
@@ -0,0 +1,34 @@
+package com.mx.curso.unidad3.Practica_Estructura_Dinamica;
+
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class ColaTareas {
+    public static void main(String[] args){
+        Queue<String>colaSolicitud = new LinkedList<>();
+
+        // Solicitudes
+        colaSolicitud.offer("Analizar sentimientos en el Tweet de Elon Musk");
+        colaSolicitud.offer("Extraer entidades nombradas en el texto");
+        colaSolicitud.offer("Crea una imagen de un oso tomando café");
+        colaSolicitud.offer("Corregir gramatica en el documento");
+        colaSolicitud.offer("Clasificar toxicidad en el comentario de la publicación");
+        colaSolicitud.offer("Generar un resumen de un artículo científico");
+
+        int total = colaSolicitud.size();
+        int restante = total;
+
+        System.out.println("Solicitudes recibidas: " + colaSolicitud);
+        System.out.println("\n---- Procesando solicitudes en orden FIFO ----");
+        System.out.println("Total de solicitudes: " + total);
+        while (!colaSolicitud.isEmpty()){
+            String solicitud = colaSolicitud.poll();
+            restante--;
+            System.out.println("Procesando solicitud: " + solicitud);
+            System.out.println("Restantes " + restante);
+        }
+
+        System.out.println("Cola después de atender las solicitudes -> " + colaSolicitud);
+    }
+}
