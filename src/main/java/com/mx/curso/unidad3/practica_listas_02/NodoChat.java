44afe041a2c0be3ff138bd7b533eff142e48fd36
Rodrigo Farid <rodrigolopezcordoba805@gmail.com>
2025-12-14 17:12:15 -0600

    Registro chat

---
+++ src/main/java/com/mx/curso/unidad3/practica_listas_02/NodoChat.java
@@ -0,0 +1,13 @@
+package com.mx.curso.unidad3.practica_listas_02;
+
+public class NodoChat {
+    public String mensaje;
+
+    public NodoChat siguiente;
+
+
+    public NodoChat(String mensaje){
+        this.mensaje = mensaje;
+        this.siguiente = null;
+    }
+}
--- src/main/java/com/mx/curso/unidad3/practica_listas_02/RegistroChat.java
+++ src/main/java/com/mx/curso/unidad3/practica_listas_02/RegistroChat.java
@@ -1,5 +1,20 @@
 package com.mx.curso.unidad3.practica_listas_02;
 
+
 public class RegistroChat {
 
+    public static void main(String[] args){
+
+        ChatBot historial = new ChatBot();
+
+        System.out.println("¡¡Bienvenido a NeuralNexus AI!!");
+        historial.insertarFinalUser("Hola, tengo una duda.");
+        historial.insertarFinalBot("Estoy aquí para ayudarte, ¿Cuál es tu duda?");
+        historial.insertarFinalUser("¿Cómo hacer un pastel?");
+        historial.insertarFinalBot("Excelente, necesitaras estos ingredientes...");
+
+        historial.imprimirChat();
+
+    }
 }
+
